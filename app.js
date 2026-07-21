a4e36c8d71b6cda6e73fb72ae2ddb433f9b192bc
ahmed-tarek <c.tarek2015@gmail.com>
2026-07-21 14:39:08 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log('Hello, World!');
+let name = 'Ahmed Tarek';
+console.log(name);
+const nameTwo = 'Ahmed Tarek 2';
+console.log(nameTwo);
+let age;
+console.log(age);
+let myString = 'Hello World';
+let myOtherString = "Hello World again";
+console.log(typeof myString);
+console.log(typeof 12.34);
+let cohort;
+console.log(cohort);
+console.log(typeof null);
+let username = 'Ahmed Tarek';
+let greeting = `Hello, ${username}!`;
+console.log(greeting);
+let longString = `Hello, ${username} and welcome to the application!
+Get started by logging in below!
+We're happy to have you. ${greeting}`;
+console.log("1"===1);
